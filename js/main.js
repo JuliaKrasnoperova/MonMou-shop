8b71ad3f0e0d9fc3865a305d1f781e7b0e692736
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2022-07-12 16:42:06 +0500

    changes

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Owl carousel
+$(document).ready(function () {
+    // Находим блок с слайдами
+    const owl = $('#products-slider');
+    // Запускаем карусель
+    owl.owlCarousel({
+        items: 3,
+        autoplay: true,
+        speed: 900,
+        autoplaySpeed: 1000,
+        loop: true,
+        dots: false
+    });
+
+    // Находим кастомные кнопки Вперед / Назад
+    const prev = $('#sliderPrev');
+    const next = $('#sliderNext');
+
+    // Клик на кнопку Назад и прокрутка карусели
+    prev.click(function () {
+        owl.trigger('prev.owl.carousel')
+    });
+    // Клик на кнопку Вперед и прокрутка карусели
+    next.click(function () {
+        owl.trigger('next.owl.carousel')
+    });
+});
